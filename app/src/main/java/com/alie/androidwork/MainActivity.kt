757679899d594ba3cc0f7284343aba816cdd6c87
AliesYangpai <yangpai_beibei@163.com>
2022-04-22 22:31:10 +0800

    test do

--- app/src/main/java/com/alie/androidwork/MainActivity.kt
+++ app/src/main/java/com/alie/androidwork/MainActivity.kt
@@ -2,10 +2,15 @@ package com.alie.androidwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+            Toast.makeText(this,"sehr gut",Toast.LENGTH_SHORT).show()
+        }
     }
 }
